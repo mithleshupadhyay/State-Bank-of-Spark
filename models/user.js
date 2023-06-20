cffba0f64bf25c8c4b2bb6ef80c9b2ea6600944e
Mithlesh Upadhyay <mithleshupadhyay515@gmail.com>
2023-06-20 20:10:42 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,27 +1,24 @@
 const mongoose = require("mongoose");
 const Transaction = require("./transaction");
 
-// Define the customer schema
-const customerSchema = new mongoose.Schema({
+const customrSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
     },
-    email: {
+    email:
+    {
         type: String,
         required: true
     },
-    balance: Number, // Customer's balance
-    transactions: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Transaction" // Reference to the Transaction model
-    }],
-    avatar: String, // Customer's avatar
-    contact: Number, // Customer's contact number
-    about: String // Information about the customer
+    Balance: Number,
+    transactions:[{
+        type: mongoose.Schema.Types.ObjectId, ref:"Transaction"}],
+    avatar:String,
+    contact:Number,
+    about:String
 });
 
-// Create the Customer model based on the schema
-const Customer = mongoose.model("Customer", customerSchema);
+const Customer = mongoose.model("Customer", customrSchema);
 
 module.exports = Customer;
