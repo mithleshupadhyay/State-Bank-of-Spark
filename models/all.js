8ee8c9958d9a3416d07c260df4a16f24e446a115
Mithlesh Upadhyay <mithleshupadhyay515@gmail.com>
2023-06-20 20:09:39 +0530

    Update all.js

--- models/all.js
+++ models/all.js
@@ -1,18 +1,10 @@
 const mongoose = require("mongoose");
 
-// Define the schema for the Datab model
 const databSchema = new mongoose.Schema({
-    date: {
-        type: Date,
-        required: true
-    },
-    payment: {
-        type: String,
-        required: true
-    }
+    Date: Date,
+    payment: String
 });
 
-// Create the Datab model based on the schema
 const Datab = mongoose.model("Datab", databSchema);
 
 module.exports = Datab;
