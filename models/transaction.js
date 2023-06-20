c706e93b3b85d97217ab3ff083a48c9c233e21e8
Mithlesh Upadhyay <mithleshupadhyay515@gmail.com>
2023-06-20 20:10:04 +0530

    Update transaction.js

--- models/transaction.js
+++ models/transaction.js
@@ -1,20 +1,12 @@
 const mongoose = require("mongoose");
 
-// Define the schema for the Transaction model
 const transactionSchema = new mongoose.Schema({
-    date: {
-        type: Date,
-        required: true
-    },
-    amount: {
-        type: Number,
-        required: true
-    },
-    description: String,
-    comment: String
+    Date:Date,
+    amount:Number,
+    Description:String,
+    comment:String
 });
 
-// Create the Transaction model based on the schema
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
 module.exports = Transaction;
